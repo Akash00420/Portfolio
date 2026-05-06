330da5065ff02d328ef32162ee9192afedc2af8a
akash <ghosakash94@gmail.com>
2026-05-06 19:37:42 +0530

    Server.js

--- Portfolio_Backend/server.js
+++ Portfolio_Backend/server.js
@@ -10,7 +10,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // handle preflight
+app.options('/{*path}', cors(corsOptions)); // handle preflight
 
 app.use(express.json());
 
